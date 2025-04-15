9ae61b1299961d8872b730582d5513e61f163920
Panagiotis Georgiadis <drpaneas@gmail.com>
2025-04-16 01:53:13 +0200

    test: clearscreen test
    Signed-off-by: Panagiotis Georgiadis <drpaneas@gmail.com>

---
+++ examples/clearscreen/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+        "image/color"
+
+        gospr64 "github.com/drpaneas/gosprite64"
+)
+
+var Azure = color.RGBA{0xf0, 0xff, 0xff, 0xff} // rgb(240, 255, 255)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+        return nil
+}
+
+func (g *Game) Draw(screen *gospr64.Screen) {
+        screen.Clear(Azure)
+}
+
+func main() {
+        // Initialize the game
+        game := &Game{}
+
+        // Run the game
+        if err := gospr64.Run(game); err != nil {
+                panic(err)
+        }
+}
